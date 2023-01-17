a6eef2a3be7867aa9cb68c1b94d6fc9f719ce6fe
Jonathan Toledo Moya <pablotoledomoya@gmail.com>
2023-01-17 10:52:12 -0300

    fix api method

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -59,3 +59,45 @@ export const getPageContents = async (location: string) => {
 
   return contentList.filter((content) => content.route === location);
 }
+
+export const getCategories = async () => {
+  const responseList  = await getData('Routes', 'A2','b20') || [];
+  const categoryList: Category[] = [];
+
+  responseList.map((category: any[]) => {
+    if (category.length > 0) {
+
+      categoryList.push({
+        idCategory: category[0],
+        nameCategory: category[1]
+      });
+    }
+
+    return category;
+  });
+
+  return categoryList;
+}
+
+export const getItems = async (idCategory: string) => {
+  const glossary  = await getData('Routes', 'c2','h20') || [];
+  const itemList: Item[] = [];
+
+  glossary.map((item: any[]) => {
+    if (item.length > 0) {
+
+      itemList.push({
+        idCategory: item[0],
+        idItem: item[1],
+        nameItem: item[2],
+        descriptionItem: item[3],
+        link: item[4],
+        image: item[5],
+      });
+    }
+
+    return item;
+  });
+
+  return itemList.filter((item) => item.idCategory === idCategory);
+}
